5b7fee0944cdd07bb0d60d31bc469943d69ea343
ivonnerubio <ivonnerubiouh@gmail.com>
2022-06-23 19:19:14 -0500

    added mismanaged wate csv

--- data/database/database.sql
+++ data/database/database.sql
@@ -11,3 +11,19 @@ COPY global_plastic_production(Entity, Code, Year,Global_plastics_production)
 FROM 'data/csv/global-plastics-production.csv'
 DELIMITER ','
 CSV HEADER;
+
+
+
+CREATE TABLE mismanaged_waste_global_total(
+    id Serial Primary Key,
+    Entity TEXT,
+    Code TEXT,
+    Year INT,
+    Mismanaged_Waste FLOAT
+);
+
+
+COPY mismanaged_waste_global_total(Entity, Code, Year,Mismanaged_Waste)
+FROM 'data/csv/global-plastics-production.csv'
+DELIMITER ','
+CSV HEADER;
