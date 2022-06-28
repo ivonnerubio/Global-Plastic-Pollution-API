83a3412e71822a6e591d468518b45cda908ab6b0
ivonnerubio <ivonnerubiouh@gmail.com>
2022-06-27 19:04:05 -0500

    deploy to heroku

--- data/database/database.js
+++ data/database/database.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
+    host: "ec2-44-206-11-200.compute-1.amazonaws.com",
+    user: "coygcxxopdzotf",
     port: 5432,
-    password: "",
-    database: "global_plastic_pollution_database"
+    password: "283198649d19785e4443eb52de491a96168ca0f6d35fe4af71319138dfe7d961",
+    database: "d2vnmmfdjvr1k"
 });
 
 
