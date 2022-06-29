3d2a225477e998ac9b6e5f081fc513c24aae0f97
ivonnerubio <ivonnerubiouh@gmail.com>
2022-06-29 18:56:32 -0500

    adding redocly docs

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const global_plastic_production_route = require('./routes/global_plastics_produc
 const mismanaged_waste_global_total_route = require('./routes/mismanaged_waste_global_total');
 const per_capita_mismanaged_route = require('./routes/per_capita_mismanaged');
 
-
 app.use('/global_plastic_production',global_plastic_production_route);
 app.use('/mismanaged_waste_global_total',mismanaged_waste_global_total_route);
 app.use('/per_capita_mismanaged',per_capita_mismanaged_route);
@@ -19,6 +18,7 @@ app.use('/per_capita_mismanaged',per_capita_mismanaged_route);
 const swaggerUi =require('swagger-ui-express');
 const YAML =require('yamljs');
 const swaggerDocument = YAML.load('routes/swagger.yaml');app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 
 app.listen(
