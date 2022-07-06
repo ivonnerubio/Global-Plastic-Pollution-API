74604bfaeb7c9d5db7a03ea315e589b9ece2333d
ivonnerubio <ivonnerubiouh@gmail.com>
2022-07-05 19:14:46 -0500

    replacing old database.js credentials to heroku

--- data/database/database.js
+++ data/database/database.js
@@ -10,11 +10,11 @@ const Pool = require('pg').Pool;
 
 
 const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
+    host: "ec2-18-215-96-22.compute-1.amazonaws.com",
+    user: "flqhnzhyvzdzpc",
     port: 5432,
-    password: "",
-    database: "global_plastic_pollution_database"
+    password: "d6d6fef78354030186e6106007484725f84adc9a8e87df328fba79d5290f13e8",
+    database: "d1m89etn4def8e"
 });
 
 
