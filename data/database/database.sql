a9858431a54f0652b816754d41e300ee57723a80
ivonnerubio <ivonnerubiouh@gmail.com>
2022-07-07 18:27:47 -0500

    created psql database

--- data/database/database.sql
+++ data/database/database.sql
@@ -38,27 +38,12 @@ CREATE TABLE per_capita_mismanaged(
     Year INT,
     Per_Capita_Mismanaged_Plastic_Waste FLOAT,
     GDP_Per_Capita FLOAT,
-    Total_Population INT,
+    Total_Population BIGINT,
     Continent TEXT
 );
 
 
-COPY per_capita_mismanaged(Entity, Code, Year,Mismanaged_Waste)
+COPY per_capita_mismanaged(Entity, Code, Year,Per_Capita_Mismanaged_Plastic_Waste,GDP_Per_Capita,Total_Population,Continent)
 FROM '/Users/ivonne/Documents/GitHub/Global-Plastic-Pollution-API/data/csv/per-capita-mismanaged-plastic-waste-vs-gdp-per-capita.csv'
-DELIMITER ','
-CSV HEADER;
-
-
--- CREATE TABLE FOR PER CAPITA PLASTIC WASTE 
-CREATE TABLE per_capita_plastic_waste(
-    id Serial Primary Key,
-    Entity TEXT,
-    Code TEXT,
-    Year INT
-);
-
-
-COPY per_capita_mismanaged(Entity, Code, Year,Mismanaged_Waste)
-FROM 'data/csv/global-plastics-production.csv'
 DELIMITER ','
 CSV HEADER;
