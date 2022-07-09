eca37257a3083ba87dc6e527b5b2a28ad79944ab
ivonnerubio <ivonnerubiouh@gmail.com>
2022-07-08 21:25:47 -0500

    editing the res.json for get Id

--- routes/global_plastics_production.js
+++ routes/global_plastics_production.js
@@ -101,21 +101,18 @@ router.get("/",async(req,res) =>{
 //     }
 // });
 
+router.get("/:id",async(req,res) =>{
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-// router.get("/:id",async(req,res) =>{
-//     const {id} = req.params;
-//     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-    
-//     pool.query("SELECT * FROM global_plastic_production WHERE id=$1",[id], (err, results) => {
-//         if (err) {
-//             console.log(err); 
-//             throw err;
-//         }
-//         res.json(record.rows[0]);
-//         });
-// });
-
-
+    const {id} = req.params;
+    pool.query("SELECT * FROM global_plastic_production WHERE id=$1",[id], (err, results) => {
+        if (err) {
+            console.log(err); 
+            throw err;
+        }
+        res.json(results.rows[0]);
+        });
+});
 
 
 
