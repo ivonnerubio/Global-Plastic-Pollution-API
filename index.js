b1e999a4dfe8e793e9330466f7f62da335a8d13e
ivonnerubio <ivonnerubiouh@gmail.com>
2022-07-10 11:59:23 -0500

    adding routes

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ app.get('/', (req, res) => {
 });
 
 const global_plastic_production_route = require('./routes/global_plastics_production.js');
+const mismanaged_waste_global_total_route = require('./routes/mismanaged_waste_global_total.js');
+const per_capita_mismanaged_route = require('./routes/per_capita_mismanaged.js');
 
 app.use("/global_plastic_production",global_plastic_production_route);
+app.use("/mismanaged_waste_global_total",mismanaged_waste_global_total_route);
+app.use("/per_capita_mismanaged",per_capita_mismanaged_route);
 
 
 
