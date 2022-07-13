6d2e97898195fc34798e38209d589f22868be1ed
ivonnerubio <ivonnerubiouh@gmail.com>
2022-07-12 19:24:11 -0500

    commenting part of the post method

--- routes/global_plastics_production.js
+++ routes/global_plastics_production.js
@@ -162,17 +162,17 @@ router.get("/:id",async(req,res) =>{
 router.post("/",async(req,res) =>{
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-    const { Entity, Code, Year, Global_plastics_production} = req.body;
+    // const { Entity, Code, Year, Global_plastics_production} = req.body;
 
-    pool.query(`INSERT INTO global_plastic_production (Entity,Code,Year,Global_plastics_production) VALUES ($1, $2, $3, $4)`, 
-        [Entity, Code, Year, Global_plastics_production], 
-        (err, results) => {
-        if (err) {
-            console.log(err); 
-            throw err;
-        }
-        res.send("Record sucessfully added")
-        });
+    // pool.query(`INSERT INTO global_plastic_production (Entity,Code,Year,Global_plastics_production) VALUES ($1, $2, $3, $4)`, 
+    //     [Entity, Code, Year, Global_plastics_production], 
+    //     (err, results) => {
+    //     if (err) {
+    //         console.log(err); 
+    //         throw err;
+    //     }
+    //     res.send("Record sucessfully added")
+    //     });
 });
 
 
