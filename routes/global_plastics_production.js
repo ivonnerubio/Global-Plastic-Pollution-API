cd9791c504259f3ade620428ae001e873d8e5173
ivonnerubio <ivonnerubiouh@gmail.com>
2022-07-16 14:42:25 -0500

    adding better respond messages

--- routes/global_plastics_production.js
+++ routes/global_plastics_production.js
@@ -208,7 +208,7 @@ router.patch("/:id",async(req,res) =>{
             console.log(err); 
             throw err;
         }
-        res.send("Record updated successfully!");
+        res.send("Entity updated successfully!");
     })}
 
     if(code){
@@ -219,7 +219,7 @@ router.patch("/:id",async(req,res) =>{
             console.log(err); 
             throw err;
         }
-        res.send("Record updated successfully!");
+        res.send("Code updated successfully!");
     })}
 
     if(year){
@@ -230,7 +230,7 @@ router.patch("/:id",async(req,res) =>{
             console.log(err); 
             throw err;
         }
-        res.send("Record updated successfully!");
+        res.send("Year updated successfully!");
     })}
 
     if(global_plastics_production){
@@ -241,7 +241,7 @@ router.patch("/:id",async(req,res) =>{
             console.log(err); 
             throw err;
         }
-        res.send("Record updated successfully!");
+        res.send("Global Plastics Production updated successfully!");
     })}
 });
 
